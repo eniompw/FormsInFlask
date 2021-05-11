9bc3a524c255885089ea8998b64a63da05ce6cba
eniompw <71254710+eniompw@users.noreply.github.com>
2021-05-11 17:45:47 +0100

    Update signup.py

--- signup.py
+++ signup.py
@@ -4,4 +4,5 @@ def insert():
     cur = con.cursor()
     cur.execute("INSERT INTO Users VALUES (?,?)",
         (request.form['un'],request.form['pw']))
+    con.commit()
     return request.form['un'] + ' added'
