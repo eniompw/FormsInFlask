f7ad63178e690592d423e1d5b320150055e4cb0b
eniompw <71254710+eniompw@users.noreply.github.com>
2021-05-11 17:33:36 +0100

    Create signup.py

---
+++ signup.py
@@ -0,0 +1,7 @@
+@app.route('/insert', methods=['POST'])
+def insert():
+    con = sqlite3.connect('login.db')
+    cur = con.cursor()
+    cur.execute("INSERT INTO Users VALUES (?,?)",
+        (request.form['un'],request.form['pw']))
+    return request.form['un'] + ' added'
